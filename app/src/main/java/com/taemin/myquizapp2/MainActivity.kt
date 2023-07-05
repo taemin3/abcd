88596862a061d0b227d69b8da618df3600ef5bad
taemin Kim <ruhokim1@naver.com>
2023-07-05 19:59:01 +0900

    second commit

--- app/src/main/java/com/taemin/myquizapp2/MainActivity.kt
+++ app/src/main/java/com/taemin/myquizapp2/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         val etName : EditText = findViewById(R.id.et_name)
         btnStart.setOnClickListener {
             if(etName.text.isEmpty()) {
-                Toast.makeText(this,"Please enter your name", Toast.LENGTH_LONG).show()
+                Toast.makeText(this,"Please enter your name22", Toast.LENGTH_LONG).show()
             }else{
                 val intent = Intent(this, QuizQuestionsActivity::class.java)
                 intent.putExtra(Constants.USER_NAME, etName.text.toString())
